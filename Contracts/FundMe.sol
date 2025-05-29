a9f3bdc7e7ddd95ac49fbcfae070235b5f5e2a38
Mavdavis <ekwesimarvelous@gmail.com>
2025-05-29 20:04:51 +0100

    Update FundMe.sol

--- Contracts/FundMe.sol
+++ Contracts/FundMe.sol
@@ -7,16 +7,19 @@ import {PriceConverter} from "../Library/PriceConverter.sol";
 
 contract FundMe{
     using PriceConverter for uint256;
+    address immutable i_owner;
      AggregatorV3Interface internal dataFeed;
     constructor() {
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
+        i_owner = msg.sender;
     }
 
   
-
-    uint public minimumUSD = 5 * 1e18;
+    // constant are used when value of an element won't chang
+    // immutables are used when they are changed once
+    uint256 public constant minimumUSD = 5 * 1e18;
     address[] public funders;
     mapping (address funder => uint256 amountFunded) public addressTofundAmount;
 
@@ -26,14 +29,19 @@ contract FundMe{
         addressTofundAmount[msg.sender] += msg.value;
 
     }
-    function withdraw()public {
+    function withdraw()public onlyOwner {
         for(uint256 i = 0; i < funders.length; i++){
             address funderAddress = funders[i];
             addressTofundAmount[funderAddress] = 0;
         }
         funders = new address[](0);
-        (bool callSuccess, bytes memory dataReturned) = msg.sender.call{value: address(this).balance}("");
-
+    //    bool res = payable((msg.sender).transfer(address(this).balance);
+        (bool callSuccess, ) = msg.sender.call{value: address(this).balance}("");
+            require(callSuccess, "Withdrawal failed");
+    }
+    modifier onlyOwner(){
+        require(msg.sender == i_owner,"You cannot withdraw unless you are the owner");
+        _;
     }
 
 }
